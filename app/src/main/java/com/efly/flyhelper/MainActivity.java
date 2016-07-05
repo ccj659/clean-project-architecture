83896bca9e1435a7ce11c1a0245fb82452f377ce
flyedt <iflymac@163.com>
2016-07-05 11:11:54 +0800

    s

--- app/src/main/java/com/efly/flyhelper/MainActivity.java
+++ app/src/main/java/com/efly/flyhelper/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView;
+        TextView  textView;
 
     }
 }
